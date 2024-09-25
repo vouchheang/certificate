bbf5f159a392044e41ce9ffdf9587c7cf68c4047
vouchh <vouchhean.mom@institute.pse.ngo>
2024-09-25 16:23:38 +0700

    first commit

--- src/components/CdnFooter.tsx
+++ src/components/CdnFooter.tsx
@@ -1,16 +1,16 @@
-"use client";
-
-import { usePathname } from "next/navigation";
+import { headers } from 'next/headers';
 import Footer from "./Footer";
 
 const ConditionalFooter = () => {
-  const pathname = usePathname();
+  const headersList = headers();
+  const pathname = headersList.get('x-invoke-path') || '';
+  
   const skipHeaderRoutes = ["/update-password", "/forgot-password", "/verify-OTP"];
-
+  
   if (skipHeaderRoutes.includes(pathname)) {
     return null;
   }
-
+  
   return (
     <>
       <Footer />
--- src/components/CdnInfo.tsx
+++ src/components/CdnInfo.tsx
@@ -1,16 +1,16 @@
-"use client";
-
-import { usePathname } from "next/navigation";
+import { headers } from 'next/headers';
 import Info from "./Info";
 
 const ConditionalFooter = () => {
-  const pathname = usePathname();
+  const headersList = headers();
+  const pathname = headersList.get('x-invoke-path') || '';
+  
   const skipHeaderRoutes = ["/login", "/register", "/update-password", "/forgot-password", "/verify-OTP"];
-
+  
   if (skipHeaderRoutes.includes(pathname)) {
     return null;
   }
-
+  
   return (
     <>
       <Info />
